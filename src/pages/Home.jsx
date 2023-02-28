6fe2050b6ce7df69dd9f6492911aff0251ca634b
Moyinoluwa-10 <moyinadelowo@gmail.com>
2023-02-28 22:41:35 +0100

    feat: added about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "../components/About";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div className="homepage">
       <Header />
+      <About />
       <Footer />
     </div>
   );
