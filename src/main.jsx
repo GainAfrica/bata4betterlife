1c9fe83fa5933dd4e3c2be26142b8ffe097e2926
Boluwatife <boluwatife1904@gmail.com>
2023-08-16 11:08:05 +0100

    feat: added scrollToTop

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
+    <ScrollToTop />
     <App />
   </Router>
 );
